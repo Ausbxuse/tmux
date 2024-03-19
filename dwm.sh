0ece98dcfc77a055fb367f592dae06b25816b70e
Zhenyu Zhao <peterzhaozzy@gmail.com>
2024-03-18 22:26:14 -0700

    update split behavior

--- dwm.sh
+++ dwm.sh
@@ -6,11 +6,11 @@ pane_count=$(tmux list-panes | wc -l)
 if [ "$1" = "open" ]; then
 	# If there is only one pane, split horizontally and resize
 	if [ "$pane_count" -eq 1 ]; then
-		tmux split-window -h
+		tmux split-window -h -c "#{pane_current_path}"
 		# Calculate half the window's width and resize the pane
 	else
 		# If there are multiple panes, split vertically
-		tmux split-window -v
+		tmux split-window -v -c "#{pane_current_path}"
 	fi
 else
 	tmux kill-pane
